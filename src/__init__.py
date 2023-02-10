79980fa2ff30460cbca1bd85850f9392654b0b75
ocPaul <stranz190097@sr.htlweiz.at>
2023-02-10 12:08:22 +0100

    Test the database

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .databaseControl import DataBaseControl
+from .datamodel import Base
+from .webBrowser import WebBrowser
+from .engine import Engine
+
+export = [DataBaseControl, Base, WebBrowser, Engine]
